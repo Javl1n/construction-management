c88a0ad8a36fcba6a7d0a27887006a8572fab874
Frank Leimbergh D. Armodia <farmodia@gmail.com>
2026-05-05 01:50:11 +0800

    added empty project component

---
+++ resources/js/components/project/empty.tsx
@@ -0,0 +1,24 @@
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty';
+import { FolderClosed } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default function EmptyProject() {
+    return (
+        <Empty>
+            <EmptyHeader>
+                <EmptyMedia variant={'icon'}>
+                    <FolderClosed />
+                </EmptyMedia >
+                <EmptyTitle>No Projects Yet</EmptyTitle>
+                <EmptyDescription>
+                    You haven&apos;t created any projects yet. Get started by creating your first project.
+                </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+                <Button>
+                    Create Project
+                </Button>
+            </EmptyContent>
+        </Empty>
+    )
+}
